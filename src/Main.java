3de2926cdefde73009ce7482f1345a12a19fd3d0
RS-gty <18920702372@163.com>
2024-09-16 22:10:11 +0800

    class addition

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import figure.Dot;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dot d1 = new Dot(1, 1, 1);
+        System.out.println(d1.getDistance());
     }
 }
--- src/figure/Dot.java
+++ src/figure/Dot.java
@@ -1,4 +1,17 @@
 package figure;
 
+import java.lang.Math;
+
 public class Dot {
+    public int x,y,z ;
+
+    public Dot(int px, int py, int pz) {
+        this.x = px;
+        this.y = py;
+        this.z = pz;
+    }
+
+    public double getDistance() {
+        return Math.sqrt(x*x + y*y + z*z);
+    }
 }
