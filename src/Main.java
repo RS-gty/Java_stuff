1d8359f79da170983a036dc73790614858bfb63d
RS-gty <18920702372@163.com>
2024-09-17 11:49:28 +0800

    Camera Feature

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import camera.Camera;
 import data.enums;
 import linalg.Vector3d;
 import linalg.matrix.RotationMatrix3d;
 
 public class Main {
     public static void main(String[] args) {
-        RotationMatrix3d rotation = new RotationMatrix3d().setRotationAxis(enums.Axis.X).setRotationAngle(Math.PI / 2);
+        Camera camera = new Camera(new Vector3d(0, 0, 0), 1, 1, 1);
         Vector3d v = new Vector3d(1, 1, 0);
-        System.out.println(rotation.multiply(v).array[2]);
+        long stime = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            camera.isVisible(v);
+        }
+        long etime = System.currentTimeMillis();
+        System.out.printf("执行时长：%d 毫秒.", (etime - stime));
     }
 }
--- src/camera/Camera.java
+++ src/camera/Camera.java
@@ -9,7 +9,50 @@ public class Camera {
     public Vector3d position;
     public double yaw, pitch, roll;
     public double fov;
+    public double[] ratio;
 
-    public RotationMatrix3d rotation = new RotationMatrix3d().setRotationAxis(enums.Axis.X).setRotationAngle(1);
-    public Matrix3d matrix = rotation;
+    /* Default target vector : (1, 0, 0) */
+    public RotationMatrix3d yaw_rotation = new RotationMatrix3d().setRotationAxis(enums.Axis.Z).setRotationAngle(-yaw);
+    public RotationMatrix3d pitch_rotation = new RotationMatrix3d().setRotationAxis(enums.Axis.Y).setRotationAngle(-pitch);
+    public RotationMatrix3d roll_rotation = new RotationMatrix3d().setRotationAxis(enums.Axis.X).setRotationAngle(-roll);
+
+    public Camera(){
+        position = new Vector3d(0,0,0);
+        yaw = 0;
+        pitch = 0;
+        roll = 0;
+        fov = 100*Math.PI/180;
+        ratio = new double[]{16, 9};
+    }
+    public Camera(Vector3d position, double yaw, double pitch, double roll, double fov, double[] ratio){
+        this.position = position;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.roll = roll;
+        this.fov = fov;
+        this.ratio = ratio;
+    }
+    public Camera(Vector3d position){
+        this.position = position;
+        this.yaw = 0;
+        this.pitch = 0;
+        this.roll = 0;
+        this.fov = 100*Math.PI/180;
+        ratio = new double[]{16, 9};
+    }
+    public Camera(Vector3d position, double yaw, double pitch, double roll){
+        this.position = position;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.roll = roll;
+        this.fov = 100*Math.PI/180;
+        ratio = new double[]{16, 9};
+    }
+
+    public boolean isVisible(Vector3d Tposition){
+        Vector3d reVector = Tposition.sub(position);
+        reVector = this.roll_rotation.multiply(this.pitch_rotation.multiply(this.yaw_rotation.multiply(reVector)));
+        double width  = reVector.x * Math.tan(fov / 2);
+        return (width >= reVector.y && width * (ratio[1] / ratio[0]) >= reVector.z);
+    }
 }
